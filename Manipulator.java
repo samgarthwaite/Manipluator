f18932c6c7527b4d1ae319f4bf2f155146968e01
samgarthwaite <43002471+samgarthwaite@users.noreply.github.com>
2018-10-28 16:17:26 -0400

    Update Manipulator.java

--- Manipulator.java
+++ Manipulator.java
@@ -13,6 +13,32 @@ public class Manipulator
 		
 		char firstLetter = str.charAt(0);
 		char secondLetter = str.charAt(str.length() - 1);
-		System.out.println("The First char is " + firstLetter + " ");
+		System.out.println("The First char is " + firstLetter + " and it is " + secondLetter + " and the length is " + str.length());
+		
+		System.out.println("Press enter to continue");
+		String hmm = Keyboard.nextLine();
+		
+		String firstHalf = str.substring(0, str.length() / 2);
+		System.out.println("The first half of the string " + firstHalf);
+		
+		System.out.println("Press enter to continue");
+		hmm = Keyboard.nextLine();
+		
+		System.out.println("Please input a char to search for");
+		String bigChar = Keyboard.nextLine();
+		System.out.println("the index where this char is " + str.indexOf(bigChar));
+		
+		System.out.println("Press enter to continue");
+		hmm = Keyboard.nextLine();
+		
+		System.out.println("Please input a starting point");
+		String startStr = Keyboard.nextLine();
+		int start = Integer.parseInt(startStr);
+		
+		System.out.println("Please input an end");
+		String endStr = Keyboard.nextLine();
+		int end = Integer.parseInt(endStr);
+		
+		System.out.println("The string you selected is " + str.substring(start, end));
 	}
 }
